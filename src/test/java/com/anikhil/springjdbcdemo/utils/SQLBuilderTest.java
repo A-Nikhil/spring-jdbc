dd6bc52b00697e6d84ae10ddc084755f0c32370b
Nikhil Anand <nikhilanand1006@gmail.com>
2021-10-08 22:38:56 +0530

    weekend push

--- src/test/java/com/anikhil/springjdbcdemo/utils/SQLBuilderTest.java
+++ src/test/java/com/anikhil/springjdbcdemo/utils/SQLBuilderTest.java
@@ -20,8 +20,30 @@ class SQLBuilderTest {
     @Test
     void buildSelectQuery() {
         String selectQuery = sqlBuilder.buildSelectQuery(tableMapping);
-        System.out.println(selectQuery);
-        Assertions.assertEquals("SELECT course_id, title, description, link FROM course", selectQuery);
+        String expectedSelectedQuery = "SELECT course_id, title, description, link FROM course";
+        Assertions.assertEquals(expectedSelectedQuery, selectQuery);
+    }
+
+    @Test
+    void buildInsertQuery() {
+        String insertQuery = sqlBuilder.buildInsertQuery(tableMapping);
+        String expectedInsertQuery = "INSERT INTO course(title, description, link) VALUES(title, description, link)";
+        Assertions.assertEquals(expectedInsertQuery, insertQuery);
+    }
+
+    @Test
+    void buildUpdateQuery() {
+
+    }
+
+    @Test
+    void buildCreateQuery() {
+
+    }
+
+    @Test
+    void buildDeleteQuery() {
+
     }
 
     @Test
