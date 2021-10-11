92f9435d986988e1973ffe3ce6972b4683d091f4
Nikhil Anand <nikhilanand1006@gmail.com>
2021-10-12 03:16:41 +0530

    added enum for data type

---
+++ src/main/java/com/anikhil/springjdbcdemo/utils/SQLDataType.java
@@ -0,0 +1,13 @@
+package com.anikhil.springjdbcdemo.utils;
+
+import com.anikhil.springjdbcdemo.validators.SQLValidator;
+
+public enum SQLDataType {
+//    STRING(SQLValidator<String>);
+//
+//    SQLDataType(SQLValidator sqlValidator) {
+//        this.sqlValidator = sqlValidator;
+//    }
+//
+//    private SQLValidator sqlValidator;
+}
