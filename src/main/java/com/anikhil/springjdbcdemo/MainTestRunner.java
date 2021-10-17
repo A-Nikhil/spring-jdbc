46ac30653dc9acd17df93d41d957e6805b6cb3aa
Nikhil Anand <nikhilanand1006@gmail.com>
2021-10-18 02:17:32 +0530

    created and tested SQLQueryBuilder

--- src/main/java/com/anikhil/springjdbcdemo/MainTestRunner.java
+++ src/main/java/com/anikhil/springjdbcdemo/MainTestRunner.java
@@ -3,23 +3,35 @@ package com.anikhil.springjdbcdemo;
 import com.anikhil.springjdbcdemo.sqltables.CourseTable;
 import com.anikhil.springjdbcdemo.sqltables.TableFactory;
 import com.anikhil.sqllib.fields.Column;
-import com.anikhil.sqllib.table.Table;
+import com.anikhil.sqllib.query.SQLQuery;
+import com.anikhil.sqllib.query.SQLQueryBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MainTestRunner {
-	private static final Logger LOG = LoggerFactory.getLogger(MainTestRunner.class);
+    private static final Logger LOG = LoggerFactory.getLogger(MainTestRunner.class);
 
-	public static void main(String[] args) {
-		testNewLibrary();
-	}
+    public static void main(String[] args) {
+        testNewLibrary();
+    }
 
-	private static boolean checkIntegerValidator() {
-		return false;
-	}
+    private static boolean checkIntegerValidator() {
+        return false;
+    }
 
-	private static void testNewLibrary() {
-		CourseTable courseTable = TableFactory.getTableByName("course", CourseTable.class);
-		Column column = courseTable.courseId;
-	}
+    private static void testNewLibrary() {
+        CourseTable courseTable = TableFactory.getTableByName("course", CourseTable.class);
+        Column[] columns = {courseTable.courseId, courseTable.title, courseTable.description, courseTable.link};
+        SQLQuery query = getCoursesFromLibTest(columns);
+        LOG.info(query.getQuery());
+    }
+
+    private static SQLQuery getCoursesFromLibTest(Column... columns) {
+        SQLQuery query = new SQLQueryBuilder()
+                .select(columns)
+                .from("course")
+                .build();
+        LOG.info(query.getQuery());
+        return query;
+    }
 }
--- src/main/java/com/anikhil/sqllib/query/SQLQueryBuilder.java
+++ src/main/java/com/anikhil/sqllib/query/SQLQueryBuilder.java
@@ -1,42 +1,59 @@
 package com.anikhil.sqllib.query;
 
+import com.anikhil.sqllib.fields.Column;
+
 public class SQLQueryBuilder {
 
-	private final SQLQuery sqlQuery;
-	private final StringBuilder queryBuilder;
+    private final SQLQuery sqlQuery;
+    private final StringBuilder queryBuilder;
 
-	public SQLQueryBuilder() {
-		this.sqlQuery = new SQLQuery();
-		this.queryBuilder = new StringBuilder();
-	}
+    public SQLQueryBuilder() {
+        this.sqlQuery = new SQLQuery();
+        this.queryBuilder = new StringBuilder();
+    }
 
-	public SQLQuery build() {
-		this.sqlQuery.setQuery(this.queryBuilder.toString());
-		this.sqlQuery.setComplete(true);
-		return this.sqlQuery;
-	}
+    public SQLQuery build() {
+        this.sqlQuery.setQuery(this.queryBuilder.toString());
+        this.sqlQuery.setComplete(true);
+        return this.sqlQuery;
+    }
 
-	public SQLQueryBuilder select() {
-		return null;
-	}
+    public SQLQueryBuilder select(Column... columns) {
+		this.queryBuilder.append("SELECT ");
+		for (Column column : columns) {
+			this.queryBuilder
+					.append(column.getColumnName())
+					.append(", ");
+		}
+		this.queryBuilder.deleteCharAt(this.queryBuilder.length() - 2);
+        return this;
+    }
 
-	public SQLQueryBuilder insert() {
-		return null;
-	}
+    public SQLQueryBuilder insert() {
+        return this;
+    }
 
-	public SQLQueryBuilder delete() {
-		return null;
-	}
+    public SQLQueryBuilder delete() {
+        return this;
+    }
 
-	public SQLQueryBuilder update() {
-		return null;
-	}
+    public SQLQueryBuilder update() {
+        return this;
+    }
 
-	public SQLQueryBuilder set() {
-		return null;
-	}
+    public SQLQueryBuilder from(String tableName) {
+        this.queryBuilder
+				.append("FROM ")
+				.append(tableName)
+				.append(" ");
+		return this;
+    }
 
-	public SQLQueryBuilder values() {
-		return null;
-	}
+    public SQLQueryBuilder set() {
+        return this;
+    }
+
+    public SQLQueryBuilder values() {
+        return this;
+    }
 }
