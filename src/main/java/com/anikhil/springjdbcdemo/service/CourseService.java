1a5dd32f457af378faaa513802adc4f2b7c7f9af
Nikhil Anand <nikhilanand1006@gmail.com>
2021-10-18 02:17:45 +0530

    integrated SQLQueryBuilder

--- src/main/java/com/anikhil/springjdbcdemo/service/CourseService.java
+++ src/main/java/com/anikhil/springjdbcdemo/service/CourseService.java
@@ -2,7 +2,6 @@ package com.anikhil.springjdbcdemo.service;
 
 import com.anikhil.springjdbcdemo.dao.BaseDAO;
 import com.anikhil.springjdbcdemo.models.Course;
-import com.anikhil.springjdbcdemo.service.BaseService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,34 +10,34 @@ import java.util.Optional;
 @Service
 public class CourseService implements BaseService<Course> {
 
-	private final BaseDAO<Course> courseBaseDAO;
+    private final BaseDAO<Course> courseBaseDAO;
 
-	public CourseService(BaseDAO<Course> courseBaseDAO) {
-		this.courseBaseDAO = courseBaseDAO;
-	}
+    public CourseService(BaseDAO<Course> courseBaseDAO) {
+        this.courseBaseDAO = courseBaseDAO;
+    }
 
-	@Override
-	public List<Course> get() {
-		return courseBaseDAO.list();
-	}
+    @Override
+    public List<Course> get() {
+        return courseBaseDAO.list();
+    }
 
-	@Override
-	public boolean create(Course course) {
-		return courseBaseDAO.create(course);
-	}
+    @Override
+    public boolean create(Course course) {
+        return courseBaseDAO.create(course);
+    }
 
-	@Override
-	public Optional<Course> getEntityWithId(int id) {
-		return courseBaseDAO.get(id);
-	}
+    @Override
+    public Optional<Course> getEntityWithId(int id) {
+        return courseBaseDAO.get(id);
+    }
 
-	@Override
-	public boolean update(Course course, int id) {
-		return courseBaseDAO.update(course, id);
-	}
+    @Override
+    public boolean update(Course course, int id) {
+        return courseBaseDAO.update(course, id);
+    }
 
-	@Override
-	public boolean deleteEntityWithId(int id) {
-		return courseBaseDAO.delete(id);
-	}
+    @Override
+    public boolean deleteEntityWithId(int id) {
+        return courseBaseDAO.delete(id);
+    }
 }
